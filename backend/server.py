12d77a337c688eef1819e7f9e6d2eebaab38a4db
mohamedazimal27 <mohamedazimal27@gmail.com>
2025-07-15 17:32:14 +0530

    feat: update production CORS policy

--- backend/server.py
+++ backend/server.py
@@ -58,7 +58,10 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://mohamedazimal.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )

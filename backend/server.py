361893deebae542d42b9bc0ad3628fbe7018675b
mohamedazimal27 <mohamedazimal27@gmail.com>
2025-12-30 23:24:40 +0530

    chore: disable database client shutdown on application exit

--- backend/server.py
+++ backend/server.py
@@ -107,6 +107,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-@app.on_event("shutdown")
-async def shutdown_db_client():
-    client.close()
+# @app.on_event("shutdown")
+# async def shutdown_db_client():
+#    client.close()
